a42ded606f70e88de997128967a5ad63a6453d1f
Joonhyo Choi <asdfchlwnsgy1236@gmail.com>
2016-12-28 13:06:35 +0900

    Misc and Script
    Minor
    - Additions
      - Added the option to toggle player model reset on death (default true).

--- EnhancedNativeTrainer/src/features/script.h
+++ EnhancedNativeTrainer/src/features/script.h
@@ -59,37 +59,31 @@ https://github.com/gtav-ent/GTAV-EnhancedNativeTrainer
 #include <fstream>
 
 //Ensures numbers are formatted with commas, not the locale option
-class comma_numpunct : public std::numpunct<char>
-{
-protected:
-	virtual char do_thousands_sep() const
-	{
+class comma_numpunct: public std::numpunct<char>{
+	protected:
+	virtual char do_thousands_sep() const{
 		return ',';
 	}
 
-	virtual std::string do_grouping() const
-	{
+	virtual std::string do_grouping() const{
 		return "\03";
 	}
 };
 
 // trim from start
-static inline std::string &ltrim(std::string &s)
-{
+static inline std::string &ltrim(std::string &s){
 	s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
 	return s;
 }
 
 // trim from end
-static inline std::string &rtrim(std::string &s)
-{
+static inline std::string &rtrim(std::string &s){
 	s.erase(std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
 	return s;
 }
 
 // trim from both ends
-static inline std::string &trim(std::string &s)
-{
+static inline std::string &trim(std::string &s){
 	return ltrim(rtrim(s));
 }
 
