4296667f6dff12ed647134de200a561043952268
Slash_Alex <sergio.slash.alex@gmail.com>
2018-12-22 17:39:58 +0500

    Fixed a critical bug in the 'Remember Wheel Angle' feature (it stopped working after the 'Arena War' update)

--- EnhancedNativeTrainer/src/features/vehicles.cpp
+++ EnhancedNativeTrainer/src/features/vehicles.cpp
@@ -2822,6 +2822,8 @@ void update_vehicle_features(BOOL bPlayerExists, Ped playerPed){
 
 ///////////////////////////// REMEMBER STEERING ANGLE //////////////////////////// ORIGINAL CODE BY MRGTAMODSGERMAN 
 	
+	if (featureVehSteerAngle && !STREAMING::HAS_MODEL_LOADED(GAMEPLAY::GET_HASH_KEY("BMX"))) STREAMING::REQUEST_MODEL(GAMEPLAY::GET_HASH_KEY("BMX"));
+
 	if (featureVehSteerAngle && PED::IS_PED_IN_ANY_VEHICLE(playerPed, true) && CONTROLS::IS_CONTROL_PRESSED(2, 75) && !PED::IS_PED_ON_ANY_BIKE(playerPed))
 	{
 		Vehicle myVehicle = PED::GET_VEHICLE_PED_IS_IN(playerPed, false);
@@ -2833,16 +2835,14 @@ void update_vehicle_features(BOOL bPlayerExists, Ped playerPed){
 			/*XPOS_2*/0.0, /*YPOS_2*/0.0, /*ZPOS_2*/0.0, /*XROT*/0.0, /*YROT*/0.0, /*ZROT*/0.0, /*BREAKFORCE*/1.0, /*FIXEDROT*/true, /*P15*/false, /*COLLISION*/false, /*P17*/1, /*P18*/true);
 		ENTITY::SET_ENTITY_ALPHA(temp_object, 0, 0);
 		if (VEH_BLIPFLASHandENGINERUNNING_VALUES[EngineRunningIndex] < 2) WAIT(1000);
-		if (VEH_BLIPFLASHandENGINERUNNING_VALUES[EngineRunningIndex] == 2) WAIT(100); // Keep Engine Running feature compatibility line
-				
+		if (VEH_BLIPFLASHandENGINERUNNING_VALUES[EngineRunningIndex] == 2) WAIT(100); // Keep Engine Running feature compatibility line 
+		
 		ENTITY::DETACH_ENTITY(myVehicle, true, true);
-
-		VEHICLE::DELETE_VEHICLE(&temp_object);
+		VEHICLE::DELETE_VEHICLE(&temp_object);  
 	} 
-	
+
 ///////////////////////////////////////////////////////////////////////////////////
-
-
+	
 	if(bPlayerExists) {
 		if(featureVehLightsOnUpdated || did_player_just_enter_vehicle(playerPed)){
 			if(featureVehLightsOn){
