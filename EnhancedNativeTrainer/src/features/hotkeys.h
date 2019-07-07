e329ada3080292f0f9011a6c15c100fee3c6a59a
Slash_Alex <32191758+SlashAlex@users.noreply.github.com>
2019-07-07 16:08:31 +0500

    Changes:
    • Improved the 'No Whistling For Taxi' option a bit
    • Added a 'Clear Props' hotkey
    • Some bugfixes to optimize memory usage

--- EnhancedNativeTrainer/src/features/hotkeys.h
+++ EnhancedNativeTrainer/src/features/hotkeys.h
@@ -70,6 +70,7 @@ static const int HKEY_BURNOUT_SWITCHING = 141;
 static const int HKEY_ALARM_SWITCHING = 142;
 static const int HKEY_TRACTIONCONTROL_SWITCHING = 143;
 static const int HKEY_DELETE_CHECKPOINT = 144;
+static const int HKEY_CLEAR_PROPS = 145;
 
 const std::vector<HOTKEY_DEF> HOTKEY_AVAILABLE_FUNCS{
 	{ "No Function", 0 },
@@ -115,6 +116,7 @@ const std::vector<HOTKEY_DEF> HOTKEY_AVAILABLE_FUNCS{
 	{ "Toggle Vehicle Alarm", HKEY_ALARM_SWITCHING },
 	{ "Toggle Traction Control", HKEY_TRACTIONCONTROL_SWITCHING },
 	{ "Remove Waypoint Marker", HKEY_DELETE_CHECKPOINT },
+	{ "Clear Props", HKEY_CLEAR_PROPS },
 };
 
 void change_hotkey_function(int hotkey, int funcIndex);
