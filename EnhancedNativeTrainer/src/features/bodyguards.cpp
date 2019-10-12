5981f99d9cdd22f8f71a37edab8247efe729e9dd
Slash_Alex <32191758+SlashAlex@users.noreply.github.com>
2019-10-12 20:44:38 +0500

    Fixed some final swimming ability bugs in the 'Bodyguards' option

--- EnhancedNativeTrainer/src/features/bodyguards.cpp
+++ EnhancedNativeTrainer/src/features/bodyguards.cpp
@@ -664,7 +664,7 @@ void maintain_bodyguards(){
 			// bodyguards swimming ability
 			if (ENTITY::IS_ENTITY_IN_WATER(PLAYER::PLAYER_PED_ID()) == 1 && PED::IS_PED_SWIMMING(PLAYER::PLAYER_PED_ID())) {
 				Vector3 my_coords = ENTITY::GET_ENTITY_COORDS(PLAYER::PLAYER_PED_ID(), true);
-				if (ENTITY::IS_ENTITY_IN_WATER(spawnedBodyguards[i]) == 1 && !PED::IS_PED_SWIMMING(spawnedBodyguards[i])) ENTITY::SET_ENTITY_COORDS(spawnedBodyguards[i], my_coords.x + 0.8, my_coords.y + 0.8, my_coords.z - 0.5, 1, 0, 0, 1); // 1.0
+				if (/*ENTITY::IS_ENTITY_IN_WATER(spawnedBodyguards[i]) == 1 && */!PED::IS_PED_SWIMMING(spawnedBodyguards[i])) ENTITY::SET_ENTITY_COORDS(spawnedBodyguards[i], my_coords.x + (0.1 * (rand() % 30)), my_coords.y + (0.1 * (rand() % 30)), my_coords.z - 0.9, 1, 0, 0, 1); // 0.8 1.0
 			}
 			//
 			PED::SET_PED_KEEP_TASK(spawnedBodyguards[i], true);
