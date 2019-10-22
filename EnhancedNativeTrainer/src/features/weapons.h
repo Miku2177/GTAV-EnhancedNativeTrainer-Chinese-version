8467bdccc2e369e3a9c9c71809bb87372d38ccb2
Slash_Alex <32191758+SlashAlex@users.noreply.github.com>
2019-10-23 01:20:51 +0500

    Added a 'Fire Mode' option

--- EnhancedNativeTrainer/src/features/weapons.h
+++ EnhancedNativeTrainer/src/features/weapons.h
@@ -256,6 +256,8 @@ void onchange_power_punch_index(int value, SelectFromListMenuItem* source);
 
 void onchange_peds_power_punch_index(int value, SelectFromListMenuItem* source);
 
+void onchange_weapons_firemode_modifier(int value, SelectFromListMenuItem* source);
+
 void onchange_sniper_vision_modifier(int value, SelectFromListMenuItem* source);
 
 void onchange_vehicle_weapon_modifier(int value, SelectFromListMenuItem* source);
