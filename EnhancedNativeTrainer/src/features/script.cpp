229921e480e3a3116c07b507638146eeb019926b
Slash_Alex <32191758+SlashAlex@users.noreply.github.com>
2020-09-19 16:42:30 +0500

    Fixed a nasty bug in the 'Player Model' option. It could hang the game while arrested as an NPC.

--- EnhancedNativeTrainer/src/features/script.cpp
+++ EnhancedNativeTrainer/src/features/script.cpp
@@ -363,7 +363,7 @@ void check_player_model(){
 	if (!ENTITY::IS_ENTITY_DEAD(PLAYER::PLAYER_PED_ID())) CoordsWhereDied = ENTITY::GET_ENTITY_COORDS(PLAYER::PLAYER_PED_ID(), true);
 
 	if (SKINS_RESET_SKIN_ONDEATH_VALUES[ResetSkinOnDeathIdx] == 0 && manual_pressed == true) {
-		if (ENTITY::IS_ENTITY_DEAD(PLAYER::PLAYER_PED_ID()) && !featureRespawnsWhereDied) {
+		if ((ENTITY::IS_ENTITY_DEAD(PLAYER::PLAYER_PED_ID()) && !featureRespawnsWhereDied) || PLAYER::IS_PLAYER_BEING_ARRESTED(PLAYER::PLAYER_ID(), 1)) {
 			if (!found) {
 				float temp_dist = 20000.0;
 				float d_temp1 = SYSTEM::VDIST(CoordsWhereDied.x, CoordsWhereDied.y, CoordsWhereDied.z, 338.409, -1396.54, 32.5092);
