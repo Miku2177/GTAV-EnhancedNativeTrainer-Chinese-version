76f5ee1092636de3e73b5ca476354bf7b90d0cf0
Slash_Alex <32191758+SlashAlex@users.noreply.github.com>
2020-10-13 01:03:01 +0500

    Fixed some bugs in the 'Manual Respawn' option. Camera was positioned under ground sometimes.

--- EnhancedNativeTrainer/src/features/script.cpp
+++ EnhancedNativeTrainer/src/features/script.cpp
@@ -61,6 +61,8 @@ int fuelTankOffset = -1;
 
 bool skydiving = false;
 
+Vehicle temp_c_object = -1;
+
 bool dive_glasses = false;
 int ped_prop_idx = -1;
 
@@ -693,6 +695,7 @@ void update_features(){
 	if (featureNoAutoRespawn && GAMEPLAY::GET_MISSION_FLAG() == 0) {
 		if ((ENTITY::IS_ENTITY_DEAD(PLAYER::PLAYER_PED_ID()) || PLAYER::IS_PLAYER_BEING_ARRESTED(PLAYER::PLAYER_ID(), 1)) && player_died == false) {
 			manual_pressed = false;
+			if (!STREAMING::HAS_MODEL_LOADED(GAMEPLAY::GET_HASH_KEY("bot_01b_bit_03"))) STREAMING::REQUEST_MODEL(GAMEPLAY::GET_HASH_KEY("bot_01b_bit_03"));
 			SCRIPT::SET_NO_LOADING_SCREEN(true);
 			GAMEPLAY::SET_FADE_OUT_AFTER_DEATH(false);
 			GAMEPLAY::SET_FADE_OUT_AFTER_ARREST(false);
@@ -704,12 +707,13 @@ void update_features(){
 		}
 		// camera rotation
 		if (ENTITY::IS_ENTITY_DEAD(PLAYER::PLAYER_PED_ID()) || PLAYER::IS_PLAYER_BEING_ARRESTED(PLAYER::PLAYER_ID(), 1)) {
-			Vector3 playerPosition = CAM::_GET_GAMEPLAY_CAM_COORDS();
-			Vector3 curRotation = CAM::GET_GAMEPLAY_CAM_ROT(2);
+			Vector3 playerPosition = ENTITY::GET_ENTITY_COORDS(PLAYER::PLAYER_PED_ID(), true);
 			if (!CAM::DOES_CAM_EXIST(DeathCamM) && (CONTROLS::IS_CONTROL_PRESSED(2, 34) || CONTROLS::IS_CONTROL_PRESSED(2, 35) || CONTROLS::IS_CONTROL_PRESSED(2, 32) || CONTROLS::IS_CONTROL_PRESSED(2, 33)) && first_person_rotate == false) {
-				DeathCamM = CAM::CREATE_CAM_WITH_PARAMS("DEFAULT_SCRIPTED_FLY_CAMERA", playerPosition.x, playerPosition.y, playerPosition.z, curRotation.x, curRotation.y, curRotation.z, 50.0, true, 2);
-				CAM::ATTACH_CAM_TO_PED_BONE(DeathCamM, PLAYER::PLAYER_PED_ID(), 31086, 0, -0.15, 3, 1); // 4
-				CAM::POINT_CAM_AT_PED_BONE(DeathCamM, PLAYER::PLAYER_PED_ID(), 31086, 0, 0.0, 0, 1);
+				temp_c_object = OBJECT::CREATE_OBJECT(GAMEPLAY::GET_HASH_KEY("bot_01b_bit_03"), playerPosition.x, playerPosition.y, playerPosition.z + 1, 1, true, 1);
+				ENTITY::SET_ENTITY_ALPHA(temp_c_object, 0, 0);
+				ENTITY::FREEZE_ENTITY_POSITION(temp_c_object, true);
+				DeathCamM = CAM::CREATE_CAM_WITH_PARAMS("DEFAULT_SCRIPTED_FLY_CAMERA", playerPosition.x, playerPosition.y, playerPosition.z + 1, 0, 0, 0, 60.0, true, 2);
+				CAM::ATTACH_CAM_TO_ENTITY(DeathCamM, temp_c_object, 0, 0.0, 0.0, 1);
 				CAM::_SET_CAM_DOF_MAX_NEAR_IN_FOCUS_DISTANCE_BLEND_LEVEL(DeathCamM, 1.0);
 				CAM::_SET_CAM_DOF_MAX_NEAR_IN_FOCUS_DISTANCE(DeathCamM, 1.0);
 				CAM::_SET_CAM_DOF_FOCUS_DISTANCE_BIAS(DeathCamM, 1.0);
@@ -719,11 +723,11 @@ void update_features(){
 			}
 			if (CAM::DOES_CAM_EXIST(DeathCamM)) { // camera rotation
 				Vector3 rot_cam = CAM::GET_CAM_ROT(DeathCamM, 2);
+				ENTITY::FREEZE_ENTITY_POSITION(temp_c_object, true);
 				if ((CONTROLS::IS_CONTROL_PRESSED(2, 34) || CONTROLS::IS_CONTROL_PRESSED(2, 35) || CONTROLS::IS_CONTROL_PRESSED(2, 32) || CONTROLS::IS_CONTROL_PRESSED(2, 33)) && first_person_rotate == false) {
 					CAM::DESTROY_CAM(DeathCamM, true);
-					DeathCamM = CAM::CREATE_CAM_WITH_PARAMS("DEFAULT_SCRIPTED_FLY_CAMERA", playerPosition.x, playerPosition.y, playerPosition.z, rot_cam.x, rot_cam.y, rot_cam.z, 50.0, true, 2);
-					CAM::ATTACH_CAM_TO_PED_BONE(DeathCamM, PLAYER::PLAYER_PED_ID(), 31086, 0, -0.15, 3, 1); // 4
-					CAM::_SET_CAM_DOF_MAX_NEAR_IN_FOCUS_DISTANCE_BLEND_LEVEL(DeathCamM, 1.0);
+					DeathCamM = CAM::CREATE_CAM_WITH_PARAMS("DEFAULT_SCRIPTED_FLY_CAMERA", playerPosition.x, playerPosition.y, playerPosition.z + 1, rot_cam.x, rot_cam.y, rot_cam.z, 60.0, true, 2);
+					CAM::ATTACH_CAM_TO_ENTITY(DeathCamM, temp_c_object, 0, 0.0, 0.0, 1);
 					CAM::_SET_CAM_DOF_MAX_NEAR_IN_FOCUS_DISTANCE(DeathCamM, 1.0);
 					CAM::_SET_CAM_DOF_FOCUS_DISTANCE_BIAS(DeathCamM, 1.0);
 					first_person_rotate = true;
@@ -737,6 +741,7 @@ void update_features(){
 		}
 		//
 		if (ENTITY::IS_ENTITY_DEAD(PLAYER::PLAYER_PED_ID()) && CONTROLS::IS_CONTROL_JUST_PRESSED(2, 22)) { // 23 // CONTROLS::IS_CONTROL_JUST_PRESSED(2, 176) ||
+			VEHICLE::DELETE_VEHICLE(&temp_c_object);
 			manual_pressed = true;
 			tick_allw = 0;
 			player_died = true;
@@ -751,6 +756,7 @@ void update_features(){
 			manual_instant = false;
 		}
 		if ((PLAYER::IS_PLAYER_BEING_ARRESTED(PLAYER::PLAYER_ID(), 1) || PLAYER::IS_PLAYER_BEING_ARRESTED(PLAYER::PLAYER_ID(), 0)) && CONTROLS::IS_CONTROL_JUST_PRESSED(2, 22)) { // CONTROLS::IS_CONTROL_JUST_PRESSED(2, 176) || 
+			VEHICLE::DELETE_VEHICLE(&temp_c_object);
 			manual_pressed = true;
 			tick_allw = 0;
 			CAM::DO_SCREEN_FADE_OUT(500);
