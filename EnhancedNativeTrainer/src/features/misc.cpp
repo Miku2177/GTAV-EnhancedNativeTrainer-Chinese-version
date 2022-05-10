3ad0a124687cacc46aaebb91fc3224b07dcffeef
Slash_Alex <32191758+SlashAlex@users.noreply.github.com>
2022-05-10 22:32:53 +0500

    Fixed a tiny bug in the 'Radio In Police Vehicle' option.

--- EnhancedNativeTrainer/src/features/misc.cpp
+++ EnhancedNativeTrainer/src/features/misc.cpp
@@ -114,7 +114,7 @@ bool featureFlyingMusic = false;
 bool featurePoliceScanner = false;
 bool featureNoComleteMessage = false;
 bool featurePoliceRadio = false;
-bool police_radio_check = false;
+//bool police_radio_check = false;
 bool featureMiscLockRadio = false;
 bool featureMiscHideHud = false;
 bool featureMiscHideHudUpdated = false;
@@ -1223,19 +1223,19 @@ void update_misc_features(BOOL playerExists, Ped playerPed){
 
 	// Radio In Police Vehicles
 	if (featurePoliceRadio) {
-		Vehicle playerVeh = PED::GET_VEHICLE_PED_IS_IN(playerPed, 1);
-		Vector3 coords_radio = ENTITY::GET_ENTITY_COORDS(playerVeh, 1);
-		Vector3 coords_radio_2 = ENTITY::GET_ENTITY_COORDS(playerPed, 1);
-		if ((PED::IS_PED_IN_ANY_POLICE_VEHICLE(playerPed) || (GAMEPLAY::GET_DISTANCE_BETWEEN_COORDS(coords_radio.x, coords_radio.y, coords_radio.z, coords_radio_2.x, coords_radio_2.y, coords_radio_2.z, false) < 15 && police_radio_check)) 
+		//Vehicle playerVeh = PED::GET_VEHICLE_PED_IS_IN(playerPed, 1);
+		//Vector3 coords_radio = ENTITY::GET_ENTITY_COORDS(playerVeh, 1);
+		//Vector3 coords_radio_2 = ENTITY::GET_ENTITY_COORDS(playerPed, 1);
+		if (/*(*/PED::IS_PED_IN_ANY_POLICE_VEHICLE(playerPed)/* || (GAMEPLAY::GET_DISTANCE_BETWEEN_COORDS(coords_radio.x, coords_radio.y, coords_radio.z, coords_radio_2.x, coords_radio_2.y, coords_radio_2.z, false) < 15 && police_radio_check))*/ 
 			&& VEHICLE::GET_IS_VEHICLE_ENGINE_RUNNING(playerVeh)) {
-			police_radio_check = true;
+			//police_radio_check = true;
 			AUDIO::SET_VEHICLE_RADIO_ENABLED(playerVeh, true);
 			AUDIO::SET_MOBILE_PHONE_RADIO_STATE(true);
 			AUDIO::SET_MOBILE_RADIO_ENABLED_DURING_GAMEPLAY(true);
 			AUDIO::SET_RADIO_AUTO_UNFREEZE(true);
 			AUDIO::SET_USER_RADIO_CONTROL_ENABLED(true);
 		}
-		if (PED::IS_PED_IN_ANY_VEHICLE(playerPed, 1)) if (!PED::IS_PED_IN_ANY_POLICE_VEHICLE(playerPed)) police_radio_check = false;
+		//if (PED::IS_PED_IN_ANY_VEHICLE(playerPed, 1)) if (!PED::IS_PED_IN_ANY_POLICE_VEHICLE(playerPed)) police_radio_check = false;
 	}
 	
 	// Freeze Radio To Station
