f35d707e6749ba9b8f0c93ad0d8d6740b9892f5a
Miku2177 <1652231043@qq.com>
2025-02-23 22:32:54 +0800

    翻译 io.cpp 文件
    此文件已全部翻译完成

--- EnhancedNativeTrainer/src/io/io.cpp
+++ EnhancedNativeTrainer/src/io/io.cpp
@@ -1,11 +1,11 @@
 /*
-Some of this code began its life as a part of GTA V SCRIPT HOOK SDK.
+这段代码最初是 GTA V SCRIPT HOOK SDK 的一部分。
 http://dev-c.com
 (C) Alexander Blade 2015
 
-It is now part of the Enhanced Native Trainer project.
+它现在是 Enhanced Native Trainer 项目的一部分。
 https://github.com/gtav-ent/GTAV-EnhancedNativeTrainer
-(C) Rob Pridham and fellow contributors 2015
+(C) Rob Pridham 和其他贡献者 2015
 */
 
 #include "io.h"
@@ -29,7 +29,7 @@ bool trainer_switch_pressed()
 	bool result = IsKeyJustUp(KeyConfig::KEY_TOGGLE_MAIN_MENU) || (CONTROLS::IS_CONTROL_PRESSED(2, controller_binds["KEY_TOGGLE_MAIN_MENU"].first) && CONTROLS::IS_CONTROL_JUST_PRESSED(2, controller_binds["KEY_TOGGLE_MAIN_MENU"].second)); //INPUT_FRONTEND_RB + INPUT_ENTER (Y)
 	if (result)
 	{
-		//avoid repeat of key press
+		//避免重复按键
 		DWORD maxTickCount = GetTickCount() + 200;
 		do
 		{
@@ -46,13 +46,13 @@ void get_button_state(bool *a, bool *b, bool *up, bool *down, bool *l, bool *r)
 	if (a) *a = IsKeyJustUp(KeyConfig::KEY_MENU_SELECT) || CONTROLS::IS_DISABLED_CONTROL_PRESSED(2, INPUT_FRONTEND_ACCEPT); //A //IS_DISABLED_CONTROL_PRESSED
 	if (b) *b = (is_menu_showing() && !IsKeyJustUp(VK_ESCAPE) && IsKeyJustUp(KeyConfig::KEY_MENU_BACK)) || (is_menu_showing() && (CONTROLS::IS_DISABLED_CONTROL_JUST_RELEASED(2, INPUT_FRONTEND_CANCEL) || CONTROLS::IS_DISABLED_CONTROL_JUST_RELEASED(2, INPUT_FRONTEND_PAUSE))); //B
 
-	if (MISC_TRAINERCONTROL_VALUES[TrainerControlIndex] == 1) { // press
+	if (MISC_TRAINERCONTROL_VALUES[TrainerControlIndex] == 1) { // 按压
 		if (up) *up = IsKeyDown(KeyConfig::KEY_MENU_UP) || CONTROLS::IS_DISABLED_CONTROL_PRESSED(2, INPUT_FRONTEND_UP); //Dpad up
 		if (down) *down = IsKeyDown(KeyConfig::KEY_MENU_DOWN) || CONTROLS::IS_DISABLED_CONTROL_PRESSED(2, INPUT_FRONTEND_DOWN); //Dpad down
 		if (r) *r = IsKeyDown(KeyConfig::KEY_MENU_RIGHT) || CONTROLS::IS_DISABLED_CONTROL_PRESSED(2, INPUT_FRONTEND_RIGHT); //Dpad right
 		if (l) *l = IsKeyDown(KeyConfig::KEY_MENU_LEFT) || CONTROLS::IS_DISABLED_CONTROL_PRESSED(2, INPUT_FRONTEND_LEFT); //Dpad left
 	}
-	if (MISC_TRAINERCONTROL_VALUES[TrainerControlIndex] == 0) { // release
+	if (MISC_TRAINERCONTROL_VALUES[TrainerControlIndex] == 0) { // 松开
 		if (up) *up = IsKeyJustUp(KeyConfig::KEY_MENU_UP) || CONTROLS::IS_DISABLED_CONTROL_JUST_RELEASED(2, INPUT_FRONTEND_UP);
 		if (down) *down = IsKeyJustUp(KeyConfig::KEY_MENU_DOWN) || CONTROLS::IS_DISABLED_CONTROL_JUST_RELEASED(2, INPUT_FRONTEND_DOWN);
 		if (r) *r = IsKeyJustUp(KeyConfig::KEY_MENU_RIGHT) || CONTROLS::IS_DISABLED_CONTROL_JUST_RELEASED(2, INPUT_FRONTEND_RIGHT);
@@ -62,7 +62,7 @@ void get_button_state(bool *a, bool *b, bool *up, bool *down, bool *l, bool *r)
 
 bool get_key_pressed(int nVirtKey)
 {
-	//return (GetKeyState(nVirtKey) & 0x8000) != 0;
+	// 返回 (GetKeyState(nVirtKey) & 0x8000) != 0;
 	return (GetAsyncKeyState(nVirtKey) & 0x8000) != 0;
 }
 
@@ -71,7 +71,7 @@ bool airbrake_switch_pressed()
 	bool result = IsKeyJustUp(KeyConfig::KEY_TOGGLE_AIRBRAKE) || (CONTROLS::IS_CONTROL_PRESSED(2, controller_binds["KEY_TOGGLE_AIRBRAKE"].first) && CONTROLS::IS_CONTROL_JUST_PRESSED(2, controller_binds["KEY_TOGGLE_AIRBRAKE"].second)); //INPUT_FRONTEND_ACCEPT (A) + INPUT_FRONTEND_RB (RB)
 	if (result)
 	{
-		//avoid repeat of key press
+		// 避免重复按键
 		DWORD maxTickCount = GetTickCount() + 200;
 		do
 		{
@@ -115,12 +115,12 @@ void setAirbrakeRelatedInputToBlocked(bool blocked, bool force)
 			function = CONTROLS::ENABLE_CONTROL_ACTION;
 		}
 		
-		function(2, 332, 1); //radio wheel up
-		function(2, 333, 1); //radio wheel down
+		function(2, 332, 1); // 收音机滚轮向上
+		function(2, 333, 1); // 收音机滚轮向下
 
 		for (int i = 81; i <= 85; i++)
 		{
-			function(2, i, 1); //radio stuff
+			function(2, i, 1); // 收音机相关的东西
 		}
 
 		gameInputBlockedByUs = blocked;
