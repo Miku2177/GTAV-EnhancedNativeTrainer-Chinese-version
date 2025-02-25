e97658282205ead771ed3c0f11209cea99c8f58f
Miku2177 <1652231043@qq.com>
2025-02-25 12:37:49 +0800

    翻译 drive_to_marker.h 文件
    已翻译

--- EnhancedNativeTrainer/src/features/drive_to_marker.h
+++ EnhancedNativeTrainer/src/features/drive_to_marker.h
@@ -13,20 +13,20 @@ extern bool featureLandAtDestination;
 extern int TelChauffeurIndex;
 extern bool TelChauffeur_Changed;
 
-//Chauffeur Speed
+// 代驾驾驶速度
 const std::vector<std::string> TEL_CHAUFFEUR_SPEED_CAPTIONS{ "20", "40", "70", "100", "120", "150", "200" }; // "30", "50", 
 const int TEL_CHAUFFEUR_SPEED_VALUES[] = { 20, 40, 70, 100, 120, 150, 200 }; // 30, 50, 
 extern int TelChauffeur_speed_IndexN;
 extern bool TelChauffeur_speed_Changed;
 
-//Chauffeur Altitude
+// 代驾驾驶高度
 const std::vector<std::string> TEL_CHAUFFEUR_ALTITUDE_CAPTIONS{ "10", "30", "50", "100", "200", "300", "500", "1000", "1500", "2000", "2500" };
 const int TEL_CHAUFFEUR_ALTITUDE_VALUES[] = { 10, 30, 50, 100, 200, 300, 500, 1000, 1500, 2000, 2500 };
 extern int TelChauffeur_altitude_Index;
 extern bool TelChauffeur_altitude_Changed;
 
-//Driving Styles
-const std::vector<std::string> TEL_CHAUFFEUR_DRIVINGSTYLES_CAPTIONS{ "Careless Driver", "Careful Driver", "Prioritise Shortcuts", "Straight To Target" };
+// 驾驶风格
+const std::vector<std::string> TEL_CHAUFFEUR_DRIVINGSTYLES_CAPTIONS{ "鲁莽驾驶", "谨慎驾驶", "优先捷径", "直奔目标" };;
 const int TEL_CHAUFFEUR_DRIVINGSTYLES_VALUES[] = { 786468, 1074528293, 262144, 16777216 };
 extern int TelChauffeur_drivingstyles_Index;
 extern bool TelChauffeur_drivingstyles_Changed;
