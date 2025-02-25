c130c5f1e8e0979c53fb57bfb5b923798d0b998a
Miku2177 <1652231043@qq.com>
2025-02-25 12:32:09 +0800

    翻译 speed_altitude.h 文件
    此文件已全部翻译完成

--- EnhancedNativeTrainer/src/features/speed_altitude.h
+++ EnhancedNativeTrainer/src/features/speed_altitude.h
@@ -6,13 +6,13 @@ extern bool featureSpeedOnFoot;
 extern bool featureSpeedOnGround;
 extern bool featureSpeedInAir;
 
-//Speedometer size
+//速度表大小
 const int SPEED_SIZE_VALUES[] = { 3, 3, 5, 7, 10, 12, 15, 17, 20, 23 };
 extern int SpeedSizeIndex;
 extern bool SizeChanged;
 
-//Speedometer position
-const std::vector<std::string> SPEED_POSITION_CAPTIONS{ "Bottom Right", "Bottom Center", "Top Right" };
+//速度表位置
+const std::vector<std::string> SPEED_POSITION_CAPTIONS{ "右下角", "底部中心", "右上角" };
 extern int SpeedPositionIndexN;
 extern bool PositionChanged;
 
